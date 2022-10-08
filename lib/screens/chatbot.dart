2bc009de62600001b6e52060526ffa997a50ddc1
brisingr24 <rawatadity234@gmail.com>
2022-10-08 17:10:52 +0530

    fixes

--- lib/screens/chatbot.dart
+++ lib/screens/chatbot.dart
@@ -11,39 +11,46 @@ class ChatBot extends StatefulWidget {
 
 class _ChatBotState extends State<ChatBot> {
   @override
+
+  void initState(){
+    super.initState();
+    _jumpHome();
+  }
+
+  _jumpHome() async{
+    await Future.delayed(const Duration(seconds: 3),(){});
+    try {
+      dynamic conversationObject = {
+        'appId': '245bd44f458f1551edc13318cb616e21c'
+      };
+      dynamic result = await KommunicateFlutterPlugin
+          .buildConversation(conversationObject);
+      print("Conversation builder success : " +
+          result.toString());
+    } on Exception catch (e) {
+      print("Conversation builder error occurred : " +
+          e.toString());
+    }
+  }
+
   Widget build(BuildContext context) {
     return Container(
-      child: ElevatedButton(
-          onPressed: () async {
-            try {
-              dynamic conversationObject = {
-                'appId': '245bd44f458f1551edc13318cb616e21c'
-              };
-              dynamic result = await KommunicateFlutterPlugin
-                  .buildConversation(conversationObject);
-              print("Conversation builder success : " +
-                  result.toString());
-            } on Exception catch (e) {
-              print("Conversation builder error occurred : " +
-                  e.toString());
-            }
-          },
-          child: const Text(
-            "Chatbot",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
-            ),
+      color: Color(0xffeae0d4),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 300,
+            width: 300,
+            child: Image.asset("images/splash.jpg"),
           ),
-          style: ButtonStyle(
-              backgroundColor:
-              MaterialStateProperty.all(Colors.black),
-              fixedSize: MaterialStateProperty.all<Size>(
-                  const Size(90, 40)),
-              shape: MaterialStateProperty.all<
-                  RoundedRectangleBorder>(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18.0),
-              )))),
+          const SizedBox(height: 30),
+          DefaultTextStyle(
+              style: TextStyle(
+                  fontSize: 30, color: Colors.green, fontFamily: 'Pacifico'),
+              child: const Text("Mindfulness")),
+        ],
+      ),
     );
   }
 }
